735ca15a2bebdaacb30ef5b6b300dc758cf8c452
NurulAfsar96 <nurulafsar.jony96@gmail.com>
2024-02-18 21:29:15 +0600

    TypeCasting

--- bin/typeCasting.dart
+++ bin/typeCasting.dart
@@ -2,14 +2,17 @@ void main()
 {
 int ab=10;
 double b= 3.1416;
-String name ='Afsar';
+String name ='Afsar123';
 String pass ='123456';
+print(b);
 
 double c= ab.toDouble();
 print(c);
 
 int pswd = int.parse(pass);
 print(pswd);
+
+print(int.tryParse(name));
 
 
 }
